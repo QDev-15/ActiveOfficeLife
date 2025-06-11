572a36649e7a4cdd8647d8920da10c1f720db6ec
Nguyễn Hữu Quỳnh <quynhvpit@outlook.com>
2025-06-11 11:22:35 +0700

    update repository

--- Backend/ActiveOfficeLife.Api/Program.cs
+++ Backend/ActiveOfficeLife.Api/Program.cs
@@ -13,6 +13,10 @@ builder.Services.AddControllers();
 builder.Services.AddScoped<ILogRepository, LogRepository>();
 builder.Services.AddScoped<ILogService, LogService>();
 builder.Services.AddHostedService<LogBackgroundService>();
+
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
