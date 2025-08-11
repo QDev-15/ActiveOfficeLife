333dd7699e363f26242988d9d78fff335d0bd052
quynhvp90 <quynh.nguyenhuu@imipgroup.com>
2025-08-11 17:04:24 +0700

    update broad cast

---
+++ Backend/ActiveOfficeLife.Admin/wwwroot/js/modules/broadcast.module.js
@@ -0,0 +1,45 @@
+﻿class BroadCastModule {
+    constructor() {
+        this._listeners = {}; // lưu sự kiện
+    }
+
+    // Đăng ký lắng nghe sự kiện
+    on(eventName, callback) {
+        if (!this._listeners[eventName]) {
+            this._listeners[eventName] = [];
+        }
+        this._listeners[eventName].push(callback);
+    }
+
+    // Gỡ bỏ lắng nghe
+    off(eventName, callback) {
+        if (!this._listeners[eventName]) return;
+        this._listeners[eventName] = this._listeners[eventName].filter(cb => cb !== callback);
+    }
+
+    // Phát sự kiện
+    emit(eventName, data) {
+        if (this._listeners[eventName]) {
+            this._listeners[eventName].forEach(cb => cb(data));
+        }
+    }
+
+    // Set token + emit event
+    setToken(token) {
+        this.token = token;
+        this.emit('tokenChanged', token);
+
+        if (!token) {
+            this.user = null;
+            this.emit('userChanged', null);
+        }
+    }
+
+    // Set user + emit event
+    setUser(user) {
+        this.user = user;
+        this.emit('userChanged', user);
+    }
+}
+
+export const broadCastInstance = new BroadCastModule();
