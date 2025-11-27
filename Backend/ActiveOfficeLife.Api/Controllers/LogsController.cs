ac00dc785cd87b8c5493bf3e80022be237c17731
quynhvp90 <quynh.nguyenhuu@imipgroup.com>
2025-11-27 11:25:11 +0700

    update logs

--- Backend/ActiveOfficeLife.Api/Controllers/LogsController.cs
+++ Backend/ActiveOfficeLife.Api/Controllers/LogsController.cs
@@ -1,8 +1,10 @@
-﻿using ActiveOfficeLife.Common.Models;
+﻿using ActiveOfficeLife.Application.Common;
+using ActiveOfficeLife.Common.Models;
 using ActiveOfficeLife.Common.Requests;
 using ActiveOfficeLife.Common.Responses;
 using ActiveOfficeLife.Domain.Entities;
 using ActiveOfficeLife.Domain.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 
@@ -38,6 +40,51 @@ namespace ActiveOfficeLife.Api.Controllers
                 PageIndex = request.PageIndex,
                 PageSize = request.PageSize
             }));
+        }
+        /// <summary>
+        /// Get logs by secret key : secret key = "123e4567-e89b-12d3-a456-426614174000"
+        /// </summary>
+        /// <param name="secret"></param>
+        /// <returns></returns>
+        [AllowAnonymous]
+        [HttpGet("secret")]
+        public async Task<IActionResult> GetBySecret([FromQuery] string key = "secret")
+        {
+            if (key != "123e4567-e89b-12d3-a456-426614174000" && key != "quynhvpit-secret")
+            {
+                return Ok(new ResultSuccess(new
+                {
+                    Items = new List<LogModel>(),
+                    TotalCount = 0,
+                    PageIndex = 1,
+                    PageSize = 1000
+                }));
+            }
+            PagingLogRequest pagingLogRequest = new PagingLogRequest
+            {
+                PageIndex = 1,
+                PageSize = 1000,
+                StartDate = DateTime.MinValue,
+                EndDate = DateTime.MaxValue
+            };
+
+            try {              
+                var logs = await _logRepository.GetAllAsync(pagingLogRequest);
+
+                return Ok(new ResultSuccess(new
+                {
+                    Items = logs.Items ?? new List<LogModel>(),
+                    TotalCount = logs.totalCount,
+                    PageIndex = pagingLogRequest.PageIndex,
+                    PageSize = pagingLogRequest.PageSize
+                }));
+            }
+            catch (Exception ex)
+            {
+                AOLLogger.Error("LogsController.GetBySecret: " + ex.Message);
+                return BadRequest(new ResultError("Invalid date format.", "400"));
+            }
+           
         }
 
     }
